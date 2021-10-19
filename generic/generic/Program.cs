bffe90d31d6f173953a2526e95fc4c7696e5d981
ummerukaya <ummerukaya13@gmail.com>
2021-10-19 17:14:34 +0600

    updated

--- generic/generic/Program.cs
+++ generic/generic/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 
-namespace generic
+namespace Generic
 {
+    class AClass<T> //generic class //T is the placeholder
+    {
+        T _number;
+        public AClass(T number)
+        {
+            this._number = number;
+        }
+
+        public T GetValue()
+        {
+            return _number;
+        }
+    }
+
+    class ANonGenericClass
+    {
+        public void GenericMethod<T>(T msg) //generic method
+        {
+            Console.WriteLine(msg);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AClass<int> IntNumber = new AClass<int>(20);
+            Console.WriteLine(IntNumber.GetValue());
+            AClass<string> StringNumber = new AClass<string>("suny");
+            Console.WriteLine(StringNumber.GetValue());
+            //here we can use one class for objects of different types
+
+            ANonGenericClass example = new ANonGenericClass();
+            example.GenericMethod<int>(20);
+            example.GenericMethod<string>("THis is a string");
+
         }
     }
 }
