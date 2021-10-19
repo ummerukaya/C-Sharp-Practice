1bfad011dc62d445f33018702ab748c19c967ada
ummerukaya <ummerukaya13@gmail.com>
2021-10-19 13:55:04 +0600

    initial commit

---
+++ sealed/sealed/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Sealed
+{
+
+    public class Animal
+    {
+        public virtual void Eat()
+        {
+            Console.WriteLine("Grass");
+        }
+    }
+
+    public class Cow : Animal
+    {
+        public sealed override void Eat()
+        {
+            Console.WriteLine("green grass");
+        }
+    }
+
+    sealed public class BabyCow : Cow
+    {
+        /*public override void Eat() //sealed method cannot be overriden //build error
+        {
+            Console.WriteLine("no grass");
+        }*/
+    }
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+            Animal animal = new Cow();
+            animal.Eat();
+    }
+}
+}
